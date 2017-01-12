2c08e99db1d02ac11778a77b1da87ee510d94eac
zankia <zankia16@gmail.com>
2017-01-12 19:23:09 +0100

    added up button fonctionnality

--- view/resources/js/script.js
+++ view/resources/js/script.js
@@ -1,8 +1,11 @@
 $(window).scroll(function() {
     var pos = $(document).scrollTop();
-    console.log(pos);
     if (pos > 0)
         $("#up").fadeIn();
     else
         $("#up").fadeOut();
 });
+
+function scrollToTop() {
+    $('body').animate({scrollTop: '0px'});
+}
--- view/resources/page/foot.php
+++ view/resources/page/foot.php
@@ -1,4 +1,4 @@
     </div>
-    <div id="up" style="display: none;"><button type="button" class="btn btn-success btn-lg"><i class="glyphicon glyicon-arrow-up" aria-hidden="true"></i></button></div>
+    <div id="up" style="display: none;"><button type="button" class="btn btn-success btn-lg" onclick="scrollToTop()"><i class="glyphicon glyphicon-arrow-up" aria-hidden="true"></i></button></div>
 </body>
 </html>
