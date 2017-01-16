173009d86b3fe63c31efcb8b49dcdb0478f3c5c0
Antoine Guillory <antoine.guillory@etu.univ-amu.fr>
2017-01-16 17:21:12 +0100

    Mise a jour du modèle CV
    Ajout de la gestion du formulaire de cv

--- model/CV.php
+++ model/CV.php
@@ -48,10 +48,32 @@
 
                 move_uploaded_file($pdf, $rep . $nom . '_' . $rename . '.' . $ext);
                 return true;
+
+                //TODO : Changer le traitement du nom du cv. Nom d'utilisateur = UTILISATEUR UNIQUE
             }
             else
             {
                 return false;
             }
+        }
+
+        public function uploadUserInformation($nickname, $name, $firstName, $socialSecuNum, $mobile,
+                                               $phone, $address, $postcode, $city, $activityDomain)
+        {
+            $query = $this->dbLink->prepare('UPDATE User SET name = :name, firstName = :firstName, socialSecuNum = :socialSecuNum,
+                                              mobile = :mobile, phone = :phone, address = :address, postcode = :postcode,
+                                              city = :city
+                                              WHERE nickname = :nickname');
+            $query->bindParam(':name', $name, PDO::PARAM_STR, 24);
+            $query->bindParam(':firstName', $firstName, PDO::PARAM_STR, 24);
+            $query->bindParam(':socialSecuNum', $socialSecuNum, PDO::PARAM_STR, 32);
+            $query->bindParam(':mobile', $mobile, PDO::PARAM_STR, 10);
+            $query->bindParam(':phone', $phone, PDO::PARAM_STR, 10);
+            $query->bindParam(':address', $address, PDO::PARAM_STR, 64);
+            $query->bindParam(':postcode', $postcode, PDO::PARAM_STR, 6);
+            $query->bindParam(':city', $city, PDO::PARAM_STR, 38);
+            $query->bindParam(':nickname', $nickname, PDO::PARAM_STR, 24);
+            $query->execute();
+
         }
     }
