9186279c70299d04bea362723b2c19b913d04b7e
Antoine Guillory <antoine.guillory@etu.univ-amu.fr>
2017-01-18 11:05:55 +0100

    Correction de Bugs + Fct de controle de données
    Correction de bugs liés a PDO 
    Ajout des différentes fonctions de contrôle des données entrées par l'utilisateur

--- model/CV.php
+++ model/CV.php
@@ -18,38 +18,15 @@
 
             if($pdfSize>0)
             {
-                $rename=rand(0,100000);
                 $rep="view/resources/stockCV/";
+                $arr = explode('.', $pdfName);
+                $ext = $arr[count($arr)-1];
+                $query = $this->dbLink->prepare('INSERT INTO CV(idUser) VALUES("' . $_SESSION['nickname'] .'")');
+                $query->execute();
+                $selection = $this->dbLink->lastInsertId();
 
-                // " " -> "_"
-                $pdfName = str_replace (" ", "_", $pdfName);
-
-                // ä, â, à -> a
-                $a = array("ä", "â", "à");
-                $pdfName = str_replace ($a, "a", $pdfName);
-
-                // é, è, ê, ë -> e
-                $e = array("é", "è", "ê", "ë");
-                $pdfName = str_replace ($e, "e", $pdfName);
-
-                // ï, î -> i
-                $i = array("ï", "î");
-                $pdfName = str_replace ($i, "i", $pdfName);
-
-                // ö, ô -> o
-                $o = array("ö", "ô");
-                $pdfName = str_replace ($o, "o", $pdfName);
-
-                // û, ù, ü -> u
-                $u = array("ù", "û", "ü");
-                $pdfName = str_replace ($u, "u", $pdfName);
-
-                list($nom, $ext) = explode(".", $pdfName);
-
-                move_uploaded_file($pdf, $rep . $nom . '_' . $rename . '.' . $ext);
+                move_uploaded_file($pdf, $rep . $selection. '.' . $ext);
                 return true;
-
-                //TODO : Changer le traitement du nom du cv. Nom d'utilisateur = UTILISATEUR UNIQUE
             }
             else
             {
@@ -58,7 +35,7 @@
         }
 
         public function uploadUserInformation($nickname, $name, $firstName, $socialSecuNum, $mobile,
-                                               $phone, $address, $postcode, $city, $activityDomain)
+                                              $phone, $address, $postcode, $city, $activityDomain)
         {
             $query = $this->dbLink->prepare('UPDATE User SET name = :name, firstName = :firstName, socialSecuNum = :socialSecuNum,
                                               mobile = :mobile, phone = :phone, address = :address, postcode = :postcode,
@@ -75,5 +52,32 @@
             $query->bindParam(':nickname', $nickname, PDO::PARAM_STR, 24);
             $query->execute();
 
+        }
+
+        public function checkPhoneValidity($phone)
+        {
+             if (!ctype_digit ($phone))
+                 return false;
+             if (strlen($phone) != 10)
+                 return false;
+             return true;
+        }
+
+        public function checkPostalValidity($postalcode)
+        {
+            if (!ctype_digit ($postalcode))
+                return false;
+            if ((strlen($postalcode) != 5) && (strlen($postalcode) != 6))
+                return false;
+            return true;
+        }
+
+        public function checkSecuValidity($secu)
+        {
+            if (!ctype_digit ($secu))
+                return false;
+            if (strlen($secu) != 15)
+                return false;
+            return true;
         }
     }
