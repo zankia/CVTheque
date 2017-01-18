4a4d8fc14607395cee96320b260b13385bfc4de0
AxelHoules <axel.houles@etu.univ-amu.fr>
2017-01-18 10:34:23 +0100

    Ajout boite a outil + changement de redirections

--- view/Stream.php
+++ view/Stream.php
@@ -2,9 +2,15 @@
 
 class StreamView extends View {
 
-
     public static function setView($view, $params = null) {
         parent::setView("Stream/" . $view, $params);
+    }
+
+    public static function CVList($params) {
+        $viewList = array("Stream/pager.php",
+                          "Stream/CVList.php",
+                          "Stream/pager.php");
+        parent::setView($viewList, $params);
     }
 
 }
--- view/resources/page/Admin/admin.php
+++ view/resources/page/Admin/admin.php
@@ -3,5 +3,5 @@
 
         <ul class="nav nav-pills nav-stacked">
             <li><a href="Admin/Userlist">Gestion des utilisateurs</a></li>
-            <li><a href="Admin/#">Gestion des CV</a></li>
+            <li><a href="Stream/CVList">Gestion des CV</a></li>
         </ul>
