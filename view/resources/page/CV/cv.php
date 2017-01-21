ed6017cf17c5794c894bc570b4a47d33aa044093
Antoine Guillory <antoine.guillory@etu.univ-amu.fr>
2017-01-21 17:06:17 +0100

    Formulaire modifié en bootstrap et suppression du domaine d'activité

--- view/resources/page/CV/cv.php
+++ view/resources/page/CV/cv.php
@@ -1,36 +1,53 @@
         <h1>Uploader un CV</h1>
           <h3>Bonjour <?php echo $_SESSION["firstName"] ? $_SESSION["firstName"] . ' ' . $_SESSION["name"] : $_SESSION["nickname"] ?>!</h3>
-             <form action="CV/ajouterCV" method="post" enctype="multipart/form-data">
-                 <p>Nom : *</p><input type="text" name="nom" id="nom" required><br/>
-                 <p>Prenom : *</p><input type="text" name="prenom" id="prenom" required><br/>
-                 <p>Numéro de sécurité sociale :</p><input type="text" name="numsecu" id="numsecu" required><br/>
-                 <!-- <input type="text" name="//TODO Assurance professionnelle :" id="assurancepro"> -->
-                <p>Tel portable : *</p><input type="tel"  name="telP" id="telP" required><br/>
-                <p>Tel fixe : *</p><input type="tel"  name="telF" id="telF" required><br/>
-                <p>Adresse :</p><input type="text" name="adresse" id="adresse"><br/>
-                <p>CP :</p><input type="number" name="codePostal" id="codePostal"><br/>
-                <p>Ville :</p><input type="text" name="ville" id="ville"><br/>
-                <p>Photo :</p><input type="file" name="photo" id="photo"><br/>
-                <p>Votre CV (en pdf) :</p><input type="file" name="pdf" id="pdf"><br/>
-                <p>Domaine d'activité :</p>
-                    <select name="domaine"><br/>
-                         <option value="agro"selected>Agroalimentaire</option>
-                         <option value="banque">Banques / Assurances</option>
-                         <option value="btp">BTP</option>
-                         <option value="chimie">Chimie</option>
-                         <option value="commerce">Commerce</option>
-                         <option value="communication">Communication</option>
-                         <option value="electro">Electronique / Electricité</option>
-                         <option value="etude">Études et conseils</option>
-                         <option value="pharma">Industrie pharmaceutique</option>
-                         <option value="info">Informatique / Télécommunications</option>
-                         <option value="machine">Machines et équipements</option>
-                         <option value="metal">Métallurgie</option>
-                         <option value="service">Services aux entreprises</option>
-                         <option value="text">Textile</option>
-                         <option value="logistique">Transports et logistique</option>
-                    </select><br/>
-                 <p>Remarques :</p><input type="remarques" name="remarques" id="remarques"><br/>
-                 <input type="submit" value="Soumettre" name="submit" class="btn btn-primary"><br/>
+             <form action="CV/addCV" method="post" enctype="multipart/form-data" class="form-horizontal">
+                 <div class="container" id="formulaire">
+                     <div class="form-group row" >
+                         <label for="nom" class="col-2 col-form-label">Nom</label>
+                         <input type="text" name="nom" id="nom" class="form-control" placeholder="Entrez votre nom" required><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label for="prenom" class="col-2 col-form-label">Prenom</label>
+                         <input type="text" name="prenom" id="prenom" class="form-control" placeholder="Entrez votre prenom" required><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label for="numsecu" class="col-2 col-form-label">Numéro de sécurité sociale</label>
+                         <input type="text" name="numsecu" id="numsecu" class="form-control" placeholder="Entrez votre N° de sécu" required><br/>
+                     </div>
+                     <!-- <input type="text" name="//TODO Assurance professionnelle :" id="assurancepro"> -->
+                     <div class="form-group row">
+                         <label for="telP" class="col-2 col-form-label">Tel portable</label>
+                         <input type="tel"  name="telP" id="telP" class="form-control" placeholder="Entrez votre N° de téléphone portable" required><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label for="telF" class="col-2 col-form-label">Tel fixe</label>
+                         <input type="tel"  name="telF" id="telF" required class="form-control" placeholder="Entrez votre N° de téléphone fixe"><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label for="adresse" class="col-2 col-form-label">Adresse</label>
+                         <input type="text" name="adresse" id="adresse" class="form-control" placeholder="Entrez votre adresse"><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label for="codePostal" class="col-2 col-form-label">Code postal</label>
+                         <input type="number" name="codePostal" id="codePostal" class="form-control" placeholder="Entrez votre adresse"><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label for="ville" class="col-2 col-form-label">Ville</label>
+                         <input type="text" name="ville" id="ville" class="form-control" placeholder="Entrez votre ville"><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label class="control-label" for="photo">Photo</label>
+                         <input type="file" name="photo" id="photo"><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label for="pdf" class="col-2 col-form-label">Votre CV (en .pdf)</label>
+                         <input type="file" name="pdf" id="pdf"><br/>
+                     </div>
+                     <div class="form-group row">
+                         <label for="remarques" class="col-2 col-form-label">Remarques</label>
+                         <input type="text" name="remarques" id="remarques"><br/>
+                     </div>
+                     <div class="form-group row">
+                         <input type="submit" value="Soumettre" name="submit" class="btn btn-primary"><br/>
+                 </div>
             </form>
-        <p>* : Ces champs sont obligatoires</p>
