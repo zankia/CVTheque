a5a2243e580faca98f6f203148f7df67d643ed4b
Antoine Guillory <antoine.guillory@etu.univ-amu.fr>
2017-01-22 21:33:53 +0100

    Revert "Validation W3C de la liste des utilisateurs"
    This reverts commit fe58ab024daccffb38981487793f6b9a453a0c9d.

--- view/resources/page/Admin/userlist.php
+++ view/resources/page/Admin/userlist.php
@@ -9,7 +9,7 @@
         <div class="panel-heading">Utilisateurs</div>
         <form class="navbar-form navbar-left" role="search" method="get" action="Admin/searchUser">
             <div class="form-group">
-                <input type="text" name="nickname" class="form-control" placeholder="Nom d'utilisateur">
+                <input type="text" name="nickname" form-control" placeholder="Nom d'utilisateur">
             </div>
             <button type="submit" class="btn btn-default">Rechercher</button>
             <a href="Admin/userList">Reinitialier la recherche</a>
@@ -56,34 +56,34 @@
                                             <div class="modal-body">
                                                 <form class="form-horizontal" action="Admin/modifyUser" method="post">
                                                     <div class="form-group">
-                                                        <label class="col-sm-3 control-label" for="name'.$i['nickname'].'">Nom</label>
+                                                        <label class="col-sm-3 control-label" for="name">Nom</label>
                                                         <div class="col-sm-9">
-                                                            <input id="name'.$i['nickname'].'" name="name" value="' . $i['name'] . '" class="form-control">
+                                                            <input id="name" name="name" value="' . $i['name'] . '" class="form-control">
                                                         </div>
                                                     </div>
                                                     <div class="form-group">
-                                                        <label class="col-sm-3 control-label" for="firstname'.$i['nickname'].'">Prenom</label>
+                                                        <label class="col-sm-3 control-label" for="firstname">Prenom</label>
                                                         <div class="col-sm-9">
-                                                            <input id="firstname'.$i['nickname'].'" name="firstname" value="' . $i['firstName'] . '"  class="form-control">
+                                                            <input id="firstname" name="firstname" value="' . $i['firstName'] . '"  class="form-control">
                                                         </div>
                                                     </div>
-                                                    <input id="id'.$i['nickname'].'" name="id" class="form-control" type="hidden" value="' . $i['nickname'] . '" pattern="[A-Za-z0-9]{3,24}" title="Au moins 3 caractères, pas de caractères spéciaux ni d\'espacement">
+                                                    <input id="id" name="id" class="form-control" type="hidden" value="' . $i['nickname'] . '" pattern="[A-Za-z0-9]{3,24}" title="Au moins 3 caractères, pas de caractères spéciaux ni d\'espacement">
                                                     <div class="form-group">
-                                                        <label class="col-sm-3 control-label" for="passwd'.$i['nickname'].'">Mot de passe*</label>
+                                                        <label class="col-sm-3 control-label" for="passwd">Mot de passe*</label>
                                                         <div class="col-sm-9">
-                                                            <input type="password" id="passwd'.$i['nickname'].'" name="passwd" class="form-control" pattern=".{8,}" title="Au moins 8 caractères">
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label class="col-sm-3 control-label" for="passwdconf'.$i['nickname'].'">Confirmation*</label>
-                                                        <div class="col-sm-9">
-                                                            <input type="password" id="passwdconf'.$i['nickname'].'" name="passwdconf" class="form-control" >
+                                                            <input type="password" id="passwd" name="passwd" class="form-control" pattern=".{8,}" title="Au moins 8 caractères">
                                                         </div>
                                                     </div>
                                                     <div class="form-group">
-                                                        <label class="col-sm-3 control-label" for="mail'.$i['nickname'].'">Adresse mail*</label>
+                                                        <label class="col-sm-3 control-label" for="passwdconf">Confirmation*</label>
                                                         <div class="col-sm-9">
-                                                            <input type="text" id="mail'.$i['nickname'].'" name="mail" value="' . $i['email'] . '"  class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+                                                            <input type="password" id="passwdconf" name="passwdconf" class="form-control" >
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label class="col-sm-3 control-label" for="mail">Adresse mail*</label>
+                                                        <div class="col-sm-9">
+                                                            <input type="email" id="mail" name="mail" value="' . $i['email'] . '"  class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
                                                         </div>
                                                     </div>
                                                     <div class="form-group">
@@ -95,7 +95,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div></td>';
+                                </div></a></td>';
 
 
     ;
